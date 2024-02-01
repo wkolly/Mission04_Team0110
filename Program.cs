73910648f11040b6becec9bea2e837818024e0b8
Justin Chew <jchew1@byu.edu>
2024-02-01 16:25:48 -0700

    changed program.cs

--- Program.cs
+++ Program.cs
@@ -1,24 +1,57 @@
 ﻿// Group 1-10 Alex P, Ryan T, Justin C, Will K
 
-internal class Program
+using System;
+
+class Program
 {
-    private static void Main(string[] args)
+    static void Main(string[] args)
     {
-        // Welcome the user to game
-        // 
-        TicToe TT = new TicToe();
-        Console.WriteLine(BoardGame());
-        Console.WriteLine("Welcome user to our Tic-Tac-Toe Game!");
+        string[,] board = new string[3, 3] { { "1", "2", "3" }, { "4", "5", "6" }, { "7", "8", "9" } };
+        string currentPlayer = "X";
+        bool gameWon = false;
+        int moves = 0;
 
-        List<int> playerMoves = new List<int>();
+        Console.WriteLine("Welcome to Tic-Tac-Toe!");
 
-        char currentPlayer = 'X';
+        while (!gameWon)
+        {
+            Console.Clear();
+            Console.WriteLine("Welcome to Tic-Tac-Toe!");
+            GameBoard.PrintBoard(board);
+            Console.WriteLine($"Player {currentPlayer}, choose a position:");
+            int choice = Convert.ToInt32(Console.ReadLine());
 
+            int row = (choice - 1) / 3;
+            int col = (choice - 1) % 3;
 
+            if (board[row, col] != "X" && board[row, col] != "O")
+            {
+                board[row, col] = currentPlayer;
+                moves++;
 
+                gameWon = GameBoard.CheckForWinner(board);
 
-        string UserInput = Console.WriteLine("Enter a number from 1-9");
+                if (gameWon)
+                {
+                    Console.Clear();
+                    GameBoard.PrintBoard(board);
+                    Console.WriteLine($"Player {currentPlayer} wins!");
+                    break;
+                }
+                else if (moves == 9)
+                {
+                    Console.Clear();
+                    GameBoard.PrintBoard(board);
+                    Console.WriteLine("It's a tie!");
+                    break;
+                }
 
-        int UserInput = Console.ReadLine();
+                currentPlayer = currentPlayer == "X" ? "O" : "X";
+            }
+            else
+            {
+                Console.WriteLine("That position is already taken. Try again.");
+            }
+        }
     }
 }
