fcbe58360adeb4893d0d0023dc0ec5525643873e
Justin Chew <jchew1@byu.edu>
2024-02-01 15:57:02 -0700

    Team10 for life

---
+++ tictactoe.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+public class GameBoard
+{
+    public static void PrintBoard(string[,] board)
+    {
+        for (int i = 0; i < board.GetLength(0); i++)
+        {
+            for (int j = 0; j < board.GetLength(1); j++)
+            {
+                Console.Write(board[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    public static bool CheckForWinner(string[,] board)
+    {
+        // Check rows
+        for (int row = 0; row < 3; row++)
+        {
+            if (board[row, 0] == board[row, 1] && board[row, 1] == board[row, 2])
+            {
+                return true;
+            }
+        }
+
+        // Check columns
+        for (int col = 0; col < 3; col++)
+        {
+            if (board[0, col] == board[1, col] && board[1, col] == board[2, col])
+            {
+                return true;
+            }
+        }
+
+        // Check diagonals
+        if (board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2] ||
+            board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0])
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
