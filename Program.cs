7a225d5856979106483099e1db506c02776a36c5
Justin Chew <jchew1@byu.edu>
2024-02-01 16:03:09 -0700

    please work

--- Program.cs
+++ Program.cs
@@ -1,20 +1,24 @@
 ﻿// Group 1-10 Alex P, Ryan T, Justin C, Will K
 
-// Welcome the user to game
-// 
-TicToe TT = new TicToe();
-Console.WriteLine(BoardGame()); 
-Console.WriteLine("Welcome user to our Tic-Tac-Toe Game!"); 
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Welcome the user to game
+        // 
+        TicToe TT = new TicToe();
+        Console.WriteLine(BoardGame());
+        Console.WriteLine("Welcome user to our Tic-Tac-Toe Game!");
 
-List<int> playerMoves = new List<int>();
+        List<int> playerMoves = new List<int>();
 
-char currentPlayer = 'X';
-
-
-
-
-string UserInput = Console.WriteLine ("Enter a number from 1-9"); 
-
-int UserInput = Console.ReadLine();
+        char currentPlayer = 'X';
 
 
+
+
+        string UserInput = Console.WriteLine("Enter a number from 1-9");
+
+        int UserInput = Console.ReadLine();
+    }
+}
